052b1fc681a6d489b912a21dd6dea8b1c5391491
Yaxin Chen <yc3995@columbia.edu>
2022-10-27 16:33:37 -0400

    rm tmp/log

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Personalizedress::Application.routes.draw do
   resources :dresses
+  resources :users
   # map '/' to be a redirect to '/dresses'
   root :to => redirect('/dresses')
   get '/search', to:"dresses#search"
+  get '/signup', to:"users#signup", as: :signup_users
+  get '/signin', to:"users#signin", as: :signin_users
 end
