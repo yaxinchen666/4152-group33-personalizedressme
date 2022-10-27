59bb893ebf94e5eb95a11c9979c66e44d768291b
yachil <57783563+yachil@users.noreply.github.com>
2022-10-27 15:48:15 -0400

    Changed Rottenpatatoes to Personalizedress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rottenpotatoes::Application.routes.draw do
+Personalizedress::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
